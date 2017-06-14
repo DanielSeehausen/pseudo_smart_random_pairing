c6e4d8685d8cae0d7f9128a0d87884accaa38ac6
github username <github email address>
2017-06-14 13:34:55 -0400

    updated script .sh

--- one_line_install.sh
+++ one_line_install.sh
@@ -1,3 +1,3 @@
 git clone git@github.com:DanielSeehausen/pseudo_smart_random_pairing.git
 chmod +x pseudo_smart_random_pairing/smart_assign.py
-echo "alias get_pairs='$PWD+/pseudo_smart_random_pairing/smart_assign.py'" >> ~/.bash_profile
+echo "alias get_pairs='$PWD/pseudo_smart_random_pairing/smart_assign.py'" >> ~/.bash_profile
