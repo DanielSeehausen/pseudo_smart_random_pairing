06dedfa520300d944666d346e61e2cada007ea08
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2017-09-05 10:26:30 -0400

    updated one line install

--- in_progress_one_line.sh
+++ in_progress_one_line.sh
@@ -1,7 +1,9 @@
+echo "...looking for Python3"
 if command -v python3 > /dev/null 2>&1; then
-  echo true
+  echo "Python3 already installed"
 else
   brew install python3
+  echo "Python3 installed"
 fi
 
 git clone git@github.com:DanielSeehausen/pseudo_smart_random_pairing.git
